6d4d42d02ccde7c32b3c7a738a69997f56574d45
Niksisking <143312300+Niksisking@users.noreply.github.com>
2026-06-21 15:11:33 +0530

    Update marketUpdater.js

--- marketUpdater.js
+++ marketUpdater.js
@@ -1,1 +1,36 @@
-console.log("SignalPro Market Updater Working");
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(
+process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+async function updateMarketData() {
+await db.collection("market").doc("live").set({
+nifty: "25120",
+niftyChange: "+0.52%",
+
+```
+bankNifty: "58240",
+bankNiftyChange: "+0.38%",
+
+sensex: "82340",
+sensexChange: "+0.44%",
+
+indiaVix: "12.43",
+indiaVixChange: "-1.20%",
+
+updatedAt: Date.now()
+```
+
+});
+
+console.log("Market Data Updated");
+}
+
+updateMarketData();
